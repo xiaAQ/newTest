530cfbf888a7d008b059d5056ab0d7ab346e0bcf
xiahaolei <1169555812@qq.com>
2020-09-29 18:23:07 +0800

    -m提交daily编写的代码

---
+++ daily_study/src/main/java/com/xiahaolei/daily_study/DailyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.xiahaolei.daily_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyStudyApplication.class, args);
+    }
+
+}
---
+++ daily_study/src/test/java/com/xiahaolei/daily_study/DailyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiahaolei.daily_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
