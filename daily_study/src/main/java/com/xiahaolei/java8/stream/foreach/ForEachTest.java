50759e2fd4e2db0ec0da1c1e56ef604fdda2b1c7
xiahaolei <1169555812@qq.com>
2020-09-29 18:37:50 +0800

    提交 测试dev

--- daily_study/src/main/java/com/xiahaolei/java8/stream/foreach/ForEachTest.java
+++ daily_study/src/main/java/com/xiahaolei/java8/stream/foreach/ForEachTest.java
@@ -3,5 +3,6 @@ package com.xiahaolei.java8.stream.foreach;
 public class ForEachTest {
     public static void main(String[] args) {
         System.out.println("打印测试");
+        System.out.println("dev打印");
     }
 }
