c85bccdb036ac22d275a8525b90eb000821f617b
xiahaolei <1169555812@qq.com>
2020-10-22 14:48:51 +0800

    提交 md5加密的测试代码

---
+++ daily_study/src/main/java/com/xiahaolei/syncAndLock/TryLockTest.java
@@ -0,0 +1,47 @@
+package com.xiahaolei.syncAndLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TryLockTest {
+    private Lock lock = new ReentrantLock();
+    //需要参与同步的方法
+    private void method(Thread thread) {
+        if (lock.tryLock()) {
+            try {
+                System.out.println("线程名" + thread.getName() + "获得了锁");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("线程名" + thread.getName() + "释放了锁");
+                lock.unlock();
+            }
+        } else {
+            System.out.println("我是" + Thread.currentThread().getName() + "有人占着锁，我就不要啦");
+        }
+    }
+
+    public static void main(String[] args) {
+        TryLockTest tryLockTest = new TryLockTest();
+
+        //线程1
+        Thread t1 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                tryLockTest.method(Thread.currentThread());
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                tryLockTest.method(Thread.currentThread());
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+    }
+}
---
+++ daily_study/src/main/java/com/xiahaolei/utils/Md5Utils.java
@@ -0,0 +1,5 @@
+package com.xiahaolei.utils;
+
+public class Md5Utils {
+
+}
