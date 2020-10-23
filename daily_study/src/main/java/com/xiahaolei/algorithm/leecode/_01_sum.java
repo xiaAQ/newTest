fbc83a82e84ea13074a8b0db7fc913aa6e1c2b18
xiahaolei <1169555812@qq.com>
2020-10-23 16:52:24 +0800

    提交 leecode的求和的代码提交

--- daily_study/src/main/java/com/xiahaolei/algorithm/leecode/_01_sum.java
+++ daily_study/src/main/java/com/xiahaolei/algorithm/leecode/_01_sum.java
@@ -14,22 +14,22 @@ public class _01_sum {
      * 空间复杂度 O(n)
      */
     public static void main(String[] args) {
-        int[] su ={2, 7, 11, 15};
+        int[] su = {2, 7, 11, 15};
         int target = 9;
-        int[] ints = twoSum(su, target);
-        Arrays.stream(ints).forEach(e-> System.out.print(e));
+        int[] ints = sum(su, target);
+        Arrays.stream(ints).forEach(e -> System.out.print(e));
     }
 
     public static int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
-        if(nums == null || nums.length == 0)
+        if (nums == null || nums.length == 0)
             return res;
         Map<Integer, Integer> map = new HashMap<>();
         int len = nums.length;
 
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             int ans = target - nums[i];
-            if(map.containsKey(ans) && map.get(ans) != i){
+            if (map.containsKey(ans) && map.get(ans) != i) {
                 res[0] = map.get(ans);
                 res[1] = i;
                 return res;
@@ -37,5 +37,19 @@ public class _01_sum {
             map.put(nums[i], i);
         }
         return res;
+    }
+
+    public static int[] sum(int[] nums, int target) {
+        int[] resp = new int[2];
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    resp[0]=i;
+                    resp[1]=j;
+                }
+            }
+        }
+        return resp;
     }
 }
